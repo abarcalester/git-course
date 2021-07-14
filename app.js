00e32676b08eb2b655aec8cbaf8571c23f2f93f5
Lester Howard <lesthoward@gmail.com>
2021-07-14 17:29:20 -0600

    One commit: sum and readme
    Sum button has been added
    
    Redme file has been modified

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const document = document.querySelector('');
+const document = document.querySelector('sum-button');
+
+const sum = document.addEventListener('click', () => {
+    return 1 + 1
+})
+
+console.log(sum);
--- readme.md
+++ readme.md
@@ -1,7 +1,3 @@
 # Objetivos del proyecto.
 
 Este proyecto consiste en dirigir los planes de la liga de la justicia. 
-
-## Misiones pendientes
-* Rescartar a Batman
-
